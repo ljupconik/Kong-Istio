161d204541a1d3dd0c13fd6fcffedb20d45ce04c
Lyupcho Nikolov <lyupcho.nikolov.-nd@disney.com>
2025-01-23 00:17:33 +0000

    The Gateway, HTTPRoute and Backends must be in the same namespace to work -  ISTIO Kong-Canary-Http-Route, all in echons namespace

--- Kong-Canary-HTTPRoute/0-label-default-ns.sh
+++ Kong-Canary-HTTPRoute/0-label-default-ns.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 istioctl install --set profile=minimal -y
+
+kubectl create namespace kong
+kubectl label ns kong istio-injection=enabled
+
+kubectl create namespace echons
+kubectl label ns echons istio-injection=enabled
+
 kubectl label ns default istio-injection=enabled
+for f in *.yaml; do kubectl apply -f $f; done
