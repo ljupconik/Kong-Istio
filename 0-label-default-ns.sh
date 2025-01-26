e10622ea03cae617e49010ae5e3fb5865d5d639e
Lyupcho Nikolov <lyupcho.nikolov.-nd@disney.com>
2025-01-26 17:02:11 +0000

    works with key-auth plugin, both with api-key or anonimous with httproute and ingress on HTTP and HTTPS even without the Cert, uses Kongs Certificate in that case

--- 0-label-default-ns.sh
+++ 0-label-default-ns.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# curl --insecure https://localhost/echo-httproute?apikey=hello_world
+# curl --insecure -H 'apikey: hello_world' https://localhost/echo-httproute
+
+# k get all -n echons
+# k get secret -n echons
+# k get kongplugin -n echons
+# k get kongconsumer -n echons
+# k get httproute -n echons
+# k get ingress -n echons
+
 # istioctl install --set profile=minimal -y
 
 # kubectl create namespace kong
@@ -14,3 +24,10 @@ kubectl create secret tls my-tls-secret \
   --key=/home/ljupco/certs/server.key
 
 for f in *.yaml; do kubectl apply -f $f; done
+
+k get all -n echons
+k get secret -n echons
+k get kongplugin -n echons
+k get kongconsumer -n echons
+k get httproute -n echons
+k get ingress -n echons
