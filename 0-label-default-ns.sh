f526294b4695b4c00a0a02a1c5f8d21493064fa1
Lyupcho Nikolov <lyupcho.nikolov.-nd@disney.com>
2025-01-26 17:39:35 +0000

    change secret name

--- 0-label-default-ns.sh
+++ 0-label-default-ns.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# curl --insecure https://localhost/echo-httproute?apikey=hello_world
-# curl --insecure -H 'apikey: hello_world' https://localhost/echo-httproute
+# curl --insecure https://localhost/echo-httproute?apikey=echo_key
+# curl --insecure -H 'apikey: echo_key' https://localhost/echo-httproute
 
 # k get all -n echons
 # k get secret -n echons
@@ -19,15 +19,18 @@
 # kubectl label ns echons istio-injection=enabled
 
 # kubectl label ns default istio-injection=enabled
-kubectl create secret tls my-tls-secret \
-  --cert=/home/ljupco/certs/server.crt \
-  --key=/home/ljupco/certs/server.key
+
+# kubectl create secret tls my-tls-secret \
+#   --cert=/home/ljupco/certs/server.crt \
+#   --key=/home/ljupco/certs/server.key
 
 for f in *.yaml; do kubectl apply -f $f; done
 
-k get all -n echons
-k get secret -n echons
-k get kongplugin -n echons
-k get kongconsumer -n echons
-k get httproute -n echons
-k get ingress -n echons
+kubectl get all -n echons
+kubectl get secret -n echons
+kubectl get kongplugin -n echons
+kubectl get kongconsumer -n echons
+kubectl get httproute -n echons
+kubectl get ingress -n echons
+kubectl get gatewayclass -n echons
+kubectl get gateway -n echons
